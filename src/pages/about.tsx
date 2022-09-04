5788cc7443208571988e7004a4e531a531055d77
Thales <hi@anhdung.me>
2022-09-04 13:52:54 +0700

    Crying

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,11 +1,11 @@
 import DocumentHead from '../components/document-head'
 import styles from '../styles/page.module.css'
 
-import createDOMPurify from 'dompurify'
-import { JSDOM } from 'jsdom'
+// import createDOMPurify from 'dompurify'
+// import { JSDOM } from 'jsdom'
 
-const window = (new JSDOM('')).window
-const DOMPurify = createDOMPurify(window)
+// const window = (new JSDOM('')).window
+// const DOMPurify = createDOMPurify(window)
 
 
 
@@ -31,7 +31,7 @@ const RenderPage = () => (
       <h2>ThoCode.com</h2>
       <img src='/image.fe1500615beccc2187c31c43cd72dac3.png' alt='Drop us an email!' />
 
-      {<div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(rawHTML) }} />}
+      {/* {<div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(rawHTML) }} />} */}
       {/* {<div dangerouslySetInnerHTML={rawHTML} />} */}
 
 
