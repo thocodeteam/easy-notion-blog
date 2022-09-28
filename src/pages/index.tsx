4387465dcfdd7ea8b0b9f8cb77d70dea20d253fc
Thales <hi@anhdung.me>
2022-09-28 10:27:02 +0700

    Modify some text in homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,9 +14,8 @@ const RenderPage = () => (
       <p>Bà con chúng ta, dù ở miền quê hay thành thị, ai cũng có thể làm được phần mềm, trước là để phục vụ nhu cầu cá nhân, sau là cho cộng đồng được thì tốt.</p>
       <h2>Our Mission</h2>
       <p>Mang lại nguồn kiến thức khoa học máy tính khô khan bằng nhiều hình thức gần gũi, thuần Việt nhất để phổ cập cho bà con.</p>
-      <p></p>
-      <p>Lời quê góp nhặt dông dài</p>
-      <p>Mua vui cũng được một trống canh</p>
+      <p></p><br />
+      <p>Lời quê góp nhặt dông dài<br />Mua vui cũng được một trống canh</p>
       <p></p>
       <p>Tạp chí Thợ Code - Code gì cũng được, miễn có lương thiện là được!</p>
     </div>
